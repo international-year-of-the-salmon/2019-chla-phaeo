ce2c0da9f75b4184af271062eec6209a87a16f95
Brett Johnson <brettthomasjohnson@gmail.com>
2021-10-26 16:48:29 -0700

    updated processing script and timezone format

--- scripts/chl_erddap_data_wrangle.R
+++ scripts/chl_erddap_data_wrangle.R
@@ -6,17 +6,18 @@ library(here)
 
 chl <- read_csv(here("original_data", "IYSchl_Hunt&Pakhomov.xlsx - Chl data.csv")) %>% 
   mutate(Date = dmy(Date),
-         dateTime = format_iso_8601(as.POSIXct(paste(Date, Time), 
+         eventDate = format_iso_8601(as.POSIXct(paste(Date, Time), 
                                                           format="%Y-%m-%d %H:%M:%S", 
                                                           tz="Asia/Kamchatka")),
-         year = year(dateTime),
-         month = month(dateTime),
-         day = day(dateTime),
-         time = hms::as_hms(ymd_hms(dateTime)),
+         eventDate = str_replace(eventDate, "\\+00:00", "Z"),
+         year = year(eventDate),
+         month = month(eventDate),
+         day = day(eventDate),
+         time = hms::as_hms(ymd_hms(eventDate)),
          cruise = "GoA2019",
          station = paste(cruise, Station, sep="_Stn"),
          profile = paste(station, "profile1", sep=":")) %>% 
-  select(cruise, station, profile, dateTime, year, month, day, time, Latitude:Longitude, Depth:Region) 
+  select(cruise, station, profile, eventDate, year, month, day, time, Latitude:Longitude, Depth:Region) 
   
 
 write_csv(chl, here("standardized_data", "IYS_NISKIN_chl_phaeo.csv"))
